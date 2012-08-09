34dd17f01496f46d13a05e06dda7a03758ab79eb
Charles D'Angelo <cdangelo@redstage.com>
2012-08-09 16:09:30 -0400

    Add controller and XML config

---
+++ code/AttributeFix/controllers/Catalog/Product/AttributeController.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'Mage/Adminhtml/controllers/Catalog/Product/AttributeController.php';
+class CGD_AttributeFix_Catalog_Product_AttributeController extends Mage_Catalog_Product_AttributeController
+{
+
+    /**
+     * Filter post data
+     *
+     * @param array $data
+     * @return array
+     */
+    protected function _filterPostData($data)
+    {
+        if ($data) {
+            /** @var $helperCatalog Mage_Catalog_Helper_Data */
+            $helperCatalog = Mage::helper('catalog');
+            //labels
+            foreach ($data['frontend_label'] as & $value) {
+                if ($value) {
+                    $value = $helperCatalog->escapeHtml($value);
+                }
+            }
+            //options
+            if (!empty($data['option']['value'])) {
+                foreach ($data['option']['value'] as &$options) {
+                    foreach ($options as &$label) {
+                        $label = $helperCatalog->stripTags($label);
+                    }
+                }
+            }
+            //default value
+            if (!empty($data['default_value'])) {
+                $data['default_value'] = $helperCatalog->escapeHtml($data['default_value']);
+            }
+            if (!empty($data['default_value_text'])) {
+                $data['default_value_text'] = $helperCatalog->escapeHtml($data['default_value_text']);
+            }
+            if (!empty($data['default_value_textarea'])) {
+                $data['default_value_textarea'] = $helperCatalog->escapeHtml($data['default_value_textarea']);
+            }
+        }
+        return $data;
+    }
+}
